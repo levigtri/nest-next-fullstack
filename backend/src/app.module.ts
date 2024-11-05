1dc90a080d715026fb080ca51a1490297f0ded52
Levi Gomes <levigft@gmail.com>
2024-11-05 15:52:24 -0300

    aplicação containerizada

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProdutoModule } from './produto/produto.module';
+import { DbModule } from './db/db.module';
+
+@Module({
+  imports: [ProdutoModule, DbModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
---
+++ backend/src/produto/dto/create-produto.dto.ts
@@ -0,0 +1,5 @@
+export default interface CreateProdutoDto {
+    nome: string;
+    descricao: string;
+    preco: number;
+}
